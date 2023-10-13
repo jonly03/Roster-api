0af395ba38855a135595172e1ba41b02afb2a2c0
Nelly Sugu <jonly03@gmail.com>
2023-10-13 08:33:10 -0400

    get ready for deployment

--- index.js
+++ index.js
@@ -6,16 +6,15 @@
 require("dotenv").config();
 const express = require("express");
 const studentsRouter = require("./routes/students");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cors());
 app.use("/api/students", studentsRouter);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("server listening");
 });
-
-console.log(process);
